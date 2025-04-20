d6fcd3220b51a48716d9591ef14c3a9a5b3eadc5
HaGaTa_ <hagatasdelus@gmail.com>
2025-04-21 03:14:30 +0900

    feat: Update url and footer

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="border-t-2 border-accent text-gray-500 dark:text-gray-300 text-xs px-8 md:px-12 lg:px-16 pt-2 flex flex-col gap-1 justify-between w-full mt-auto">
       <div className="container mx-auto">
-        <p className="flex justify-end">
+        <p className="flex justify-end whitespace-nowrap">
           <a
             href="/info"
             className="text-gray-500 dark:text-gray-300 hover:text-accent dark:hover:text-accent underline decoration-dashed underline-offset-4 transition-colors"
