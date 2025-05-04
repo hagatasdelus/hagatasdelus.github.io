6602dae165536422d3bfdb93a3a9d04d9df67539
HaGaTa_ <hagatasdelus@gmail.com>
2025-05-05 08:16:12 +0900

    :nail_care: (Footer.tsx) the layout to be comfortable on any device size

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -5,23 +5,29 @@ const currentYear = new Date().getFullYear();
 export default function Footer() {
   return (
     <footer>
-      <div className="border-t-2 border-accent text-gray-500 dark:text-gray-300 text-xs">
-        <div className="container mx-auto px-4 md:px-8 lg:px-12 pt-3">
-          <div className="text-right pb-1.5">
-            <p>
-              <span>Copyright&nbsp;&copy;&nbsp;{currentYear}&nbsp;</span>
-              <a
-                href="/about"
-                className="hover:text-accent dark:hover:text-accent underline decoration-dashed underline-offset-4"
-              >
-                Hagata
-              </a>
-              <span>.&nbsp;All rights reserved.</span>
-            </p>
+      <div className="mx-6">
+        <div className="border-t-2 border-accent">
+          <div className="container mx-auto my-1.5 px-4 md:px-8 lg:px-12 max-w-5xl">
+            <div className="flex justify-end h-16 items-center">
+              <section className="text-right">
+                <div className="text-xs text-gray-500 dark:text-gray-300 mb-3">
+                  <p>
+                    <span>Copyright&nbsp;&copy;&nbsp;{currentYear}&nbsp;</span>
+                    <a
+                      href="/about"
+                      className="hover:text-accent dark:hover:text-accent underline decoration-dashed underline-offset-4"
+                    >
+                      Hagata
+                    </a>
+                    <span>.&nbsp;All rights reserved.</span>
+                  </p>
+                </div>
+                <ul className="flex justify-end list-none space-x-4 mt-1">
+                  <SocialList />
+                </ul>
+              </section>
+            </div>
           </div>
-          <ul className="flex justify-end list-none space-x-4 my-2">
-            <SocialList />
-          </ul>
         </div>
       </div>
     </footer>
