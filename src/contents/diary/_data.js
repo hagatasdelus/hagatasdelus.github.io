48b7a7089159934519badef49d663cb419216f5b
HaGaTa_ <hagatasdelus@gmail.com>
2025-06-09 02:27:33 +0900

    feat: apply PostOGImage layout to open graph layout in diary posts

--- src/contents/diary/_data.js
+++ src/contents/diary/_data.js
@@ -3,6 +3,7 @@ import { SITE_DESCRIPTION } from "../../consts.ts";
 export const tags = ["diary"];
 
 export const layout = "layouts/Post.tsx";
+export const openGraphLayout = "layouts/PostOGImage.tsx";
 export const templateEngine = "md";
 export const metas = {
   description: SITE_DESCRIPTION,
