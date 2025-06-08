1c0da91fee5f58cbb0634007e3e86ff9b5cb621c
HaGaTa_ <hagatasdelus@gmail.com>
2025-06-09 01:52:41 +0900

    feat: apply BaseOGImage layout to open graph layout in blog.tsx

--- src/blog.tsx
+++ src/blog.tsx
@@ -1,9 +1,10 @@
-import { SITE_TITLE, SITE_DESCRIPTION } from "./consts.ts";
+import { SITE_DESCRIPTION } from "./consts.ts";
 import PostList from "./_components/PostList.tsx";
 import Search from "./_components/Search.tsx";
 
 export const layout = "layouts/Base.tsx";
-export const title = `Blog - ${SITE_TITLE}`;
+export const openGraphLayout = "layouts/BaseOGImage.tsx";
+export const title = "Blog";
 export const metas = {
   title: "=title",
   description: SITE_DESCRIPTION,
