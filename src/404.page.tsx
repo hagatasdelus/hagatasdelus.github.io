cec5802c014fe745f02caef43f1aedd54d7a7c83
HaGaTa_ <hagatasdelus@gmail.com>
2025-06-18 05:41:16 +0900

    feat: rename 404.tsx -> 404.page.tsx - lume v3

--- src/404.page.tsx
+++ src/404.page.tsx
@@ -12,7 +12,11 @@ export const metas = {
 export default function () {
   return (
     <>
-      <h2>404 Not Found</h2>
+      <div className="max-w-4xl mx-auto px-4 py-8">
+        <div className="prose md:prose-lg mx-auto md:mx-6">
+          <h1 className="text-2xl md:text-4xl font-bold mb-8">404 Not Found</h1>
+        </div>
+      </div>
     </>
   );
 }
