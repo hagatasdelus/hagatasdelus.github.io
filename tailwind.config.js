b0ec184c540e952559c915672323ef54cceb378c
HaGaTa_ <hagatasdelus@gmail.com>
2025-06-22 23:58:58 +0900

    (tailwind.config.js) comment out

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,31 @@
-import daisyui from "npm:daisyui@latest";
-import typography from "npm:@tailwindcss/typography";
+// import daisyui from "npm:daisyui@latest";
 
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/**/*.{html,md,mdx,js,jsx,ts,tsx}",
-  ],
-  darkMode: ['selector', '[data-theme="sunset"]'],
-  plugins: [daisyui, typography],
-  daisyui: {
-    themes: ["retro", "sunset"],
-  },
-  safelist: [],
-  theme: {
-    extend: {
-      typography: (_theme) => ({
-        DEFAULT: {
-          css: {
-            color: "oklch(var(--bc))",
-            a: {
-              textDecorationOffset: "4px",
-              "&:hover": {
-                color: "oklch(var(--s))",
-                textDecorationThickness: "2px",
-                transition: "all .2s ease-in-out",
-              },
-            },
-          },
-        },
-      }),
-    },
-  },
-};
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{html,md,mdx,js,jsx,ts,tsx}"],
+//   darkMode: ["selector", '[data-theme="sunset"]'],
+//   plugins: [daisyui],
+//   daisyui: {
+//     themes: ["retro", "sunset"],
+//     logs: true,
+//   },
+//   theme: {
+//     extend: {
+//       typography: (_theme) => ({
+//         DEFAULT: {
+//           css: {
+//             color: "oklch(var(--bc))",
+//             a: {
+//               textDecorationOffset: "4px",
+//               "&:hover": {
+//                 color: "oklch(var(--s))",
+//                 textDecorationThickness: "2px",
+//                 transition: "all .2s ease-in-out",
+//               },
+//             },
+//           },
+//         },
+//       }),
+//     },
+//   },
+// };
