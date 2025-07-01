79f9adfaaaffbc463c60c6d347c3c7797937f7ad
HaGaTa_ <hagatasdelus@gmail.com>
2025-07-02 07:57:21 +0900

    feat: apply blog.css

--- src/_includes/layouts/Post.tsx
+++ src/_includes/layouts/Post.tsx
@@ -25,6 +25,7 @@ export default function Post(data: Lume.Data) {
           <meta charSet="UTF-8" />
           <title>{title}</title>
           <link rel="stylesheet" href="/styles/base.css" />
+          <link rel="stylesheet" href="/styles/blog.css" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
